ad8041b2b3f7aca788fcf990db5a674d32f77f15
unknown <victor.garcia-pereda@bonitasoft.com>
2015-08-17 19:05:35 +0200

    v7 Spanish translation by Victor

---
+++ exercices/source/es/resources/code/ex2_initRequest.java
@@ -0,0 +1,16 @@
+import org.bonitasoft.engine.api.ProcessAPI;
+import org.bonitasoft.engine.bpm.process.ProcessInstance;
+
+import com.company.model.SolicitudVacaciones;
+
+// Obten la instancia del proceso actual
+ProcessAPI processAPI = apiAccessor.getProcessAPI();
+ProcessInstance processInstance = processAPI.getProcessInstance(processInstanceId);
+
+// Inicializa nueva solicitud de vacaciones
+SolicitudVacaciones nuevaSol = new SolicitudVacaciones();
+nuevaSol.setSolicitanteId(processInstance.getStartedBy());
+nuevaSol.setNumDias(solicitudContrato.get("numDias"));
+nuevaSol.setDiaInicio(solicitudContrato.get("diaInicio"));
+	
+return nuevaSol;
